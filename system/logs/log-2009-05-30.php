edbb4b19a9c7e264d7c423ee56dda63e427e0581
J. Knight <jbp@kw.igs.net>
2009-05-31 15:19:10 -0400

    added system to repo

---
+++ system/logs/log-2009-05-30.php
@@ -0,0 +1,292 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2009-05-30 13:06:57 --> Config Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> URI Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Router Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Output Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Input Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:06:57 --> Language Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:06:57 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Session Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:06:57 --> Session routines successfully run
+DEBUG - 2009-05-30 13:06:57 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:06:57 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:06:57 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> foo
+DEBUG - 2009-05-30 13:06:57 --> Model Class Initialized
+DEBUG - 2009-05-30 13:06:57 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:06:57 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:06:57 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:06:57 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:06:57 --> Final output sent to browser
+DEBUG - 2009-05-30 13:06:57 --> Total execution time: 0.1551
+DEBUG - 2009-05-30 13:08:17 --> Config Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> URI Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Router Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Output Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Input Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:08:17 --> Language Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:08:17 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Session Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:08:17 --> Session routines successfully run
+DEBUG - 2009-05-30 13:08:17 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:08:17 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:08:17 --> Auth class initialized
+DEBUG - 2009-05-30 13:08:17 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> foo
+DEBUG - 2009-05-30 13:08:17 --> Model Class Initialized
+DEBUG - 2009-05-30 13:08:17 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:08:17 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:08:17 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:08:17 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:08:17 --> Final output sent to browser
+DEBUG - 2009-05-30 13:08:17 --> Total execution time: 0.1942
+DEBUG - 2009-05-30 13:09:52 --> Config Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> URI Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Router Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Output Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Input Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:09:52 --> Language Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:09:52 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Session Class Initialized
+DEBUG - 2009-05-30 13:09:52 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:09:52 --> Session routines successfully run
+DEBUG - 2009-05-30 13:10:02 --> Config Class Initialized
+DEBUG - 2009-05-30 13:10:02 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:10:02 --> URI Class Initialized
+DEBUG - 2009-05-30 13:10:02 --> Router Class Initialized
+DEBUG - 2009-05-30 13:10:02 --> Output Class Initialized
+DEBUG - 2009-05-30 13:10:02 --> Input Class Initialized
+DEBUG - 2009-05-30 13:10:02 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:10:02 --> Language Class Initialized
+DEBUG - 2009-05-30 13:10:03 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:10:03 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:10:03 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:10:03 --> Session Class Initialized
+DEBUG - 2009-05-30 13:10:03 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:10:03 --> Session routines successfully run
+DEBUG - 2009-05-30 13:10:03 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:10:03 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:10:03 --> Auth class initialized
+DEBUG - 2009-05-30 13:10:03 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:10:03 --> Model Class Initialized
+DEBUG - 2009-05-30 13:10:03 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:10:03 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:10:03 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:10:03 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:10:03 --> Final output sent to browser
+DEBUG - 2009-05-30 13:10:03 --> Total execution time: 0.1664
+DEBUG - 2009-05-30 13:11:50 --> Config Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> URI Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Router Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Output Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Input Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:11:50 --> Language Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:11:50 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Session Class Initialized
+DEBUG - 2009-05-30 13:11:50 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:11:50 --> Session routines successfully run
+DEBUG - 2009-05-30 13:12:02 --> Config Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> URI Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Router Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Output Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Input Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:12:02 --> Language Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:12:02 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Session Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:12:02 --> Session routines successfully run
+DEBUG - 2009-05-30 13:12:02 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:12:02 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:12:02 --> Auth class initialized
+DEBUG - 2009-05-30 13:12:02 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> Model Class Initialized
+DEBUG - 2009-05-30 13:12:02 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:12:02 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:12:02 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:12:02 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:12:02 --> Final output sent to browser
+DEBUG - 2009-05-30 13:12:02 --> Total execution time: 0.1960
+DEBUG - 2009-05-30 13:13:17 --> Config Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> URI Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Router Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Output Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Input Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:13:17 --> Language Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:13:17 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Session Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:13:17 --> Session routines successfully run
+DEBUG - 2009-05-30 13:13:17 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:13:17 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:13:17 --> Auth class initialized
+DEBUG - 2009-05-30 13:13:17 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> Model Class Initialized
+DEBUG - 2009-05-30 13:13:17 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:13:17 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:13:17 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:13:17 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:13:17 --> Final output sent to browser
+DEBUG - 2009-05-30 13:13:17 --> Total execution time: 0.1783
+DEBUG - 2009-05-30 13:15:05 --> Config Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> URI Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Router Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Output Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Input Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:15:05 --> Language Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:15:05 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Session Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:15:05 --> Session routines successfully run
+DEBUG - 2009-05-30 13:15:05 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:15:05 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:15:05 --> Auth class initialized
+DEBUG - 2009-05-30 13:15:05 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> Model Class Initialized
+DEBUG - 2009-05-30 13:15:05 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:15:05 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:15:05 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:15:05 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:15:05 --> Final output sent to browser
+DEBUG - 2009-05-30 13:15:05 --> Total execution time: 0.1502
+DEBUG - 2009-05-30 13:17:10 --> Config Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> URI Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Router Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Output Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Input Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:17:10 --> Language Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:17:10 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Session Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:17:10 --> Session routines successfully run
+DEBUG - 2009-05-30 13:17:10 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:17:10 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:17:10 --> Auth class initialized
+DEBUG - 2009-05-30 13:17:10 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> Model Class Initialized
+DEBUG - 2009-05-30 13:17:10 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:17:10 --> File loaded: ../app/views/admin/calendar/calendar.php
+DEBUG - 2009-05-30 13:17:10 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:17:10 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:17:10 --> Final output sent to browser
+DEBUG - 2009-05-30 13:17:10 --> Total execution time: 0.1125
+DEBUG - 2009-05-30 13:18:15 --> Config Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> URI Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Router Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Output Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Input Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:18:15 --> Language Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:18:15 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Session Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:18:15 --> Session routines successfully run
+DEBUG - 2009-05-30 13:18:15 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:18:15 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:18:15 --> Auth class initialized
+DEBUG - 2009-05-30 13:18:15 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:18:15 --> Auth: check role: admin
+DEBUG - 2009-05-30 13:18:16 --> Config Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> URI Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Router Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Output Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Input Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:18:16 --> Language Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:18:16 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Session Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:18:16 --> Session routines successfully run
+DEBUG - 2009-05-30 13:18:16 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:18:16 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:18:16 --> Auth class initialized
+DEBUG - 2009-05-30 13:18:16 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:18:16 --> File loaded: ../app/views/admin/login.php
+DEBUG - 2009-05-30 13:18:16 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:18:16 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:18:16 --> Final output sent to browser
+DEBUG - 2009-05-30 13:18:16 --> Total execution time: 0.0359
+DEBUG - 2009-05-30 13:18:38 --> Config Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> URI Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Router Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Output Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Input Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:18:38 --> Language Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:18:38 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Session Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:18:38 --> Session routines successfully run
+DEBUG - 2009-05-30 13:18:38 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:18:38 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:18:38 --> Auth class initialized
+DEBUG - 2009-05-30 13:18:38 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Auth: request login with user: 'jim' pass: 'py31415'
+DEBUG - 2009-05-30 13:18:38 --> Config Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Hooks Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> URI Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Router Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Output Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Input Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Global POST and COOKIE data sanitized
+DEBUG - 2009-05-30 13:18:38 --> Language Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Loader Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Config file loaded: config/site_config.php
+DEBUG - 2009-05-30 13:18:38 --> Database Driver Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Session Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> Helper loaded: string_helper
+DEBUG - 2009-05-30 13:18:38 --> Session routines successfully run
+DEBUG - 2009-05-30 13:18:38 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2009-05-30 13:18:38 --> Helper loaded: url_helper
+DEBUG - 2009-05-30 13:18:38 --> Auth class initialized
+DEBUG - 2009-05-30 13:18:38 --> Controller Class Initialized
+DEBUG - 2009-05-30 13:18:38 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:18:38 --> File loaded: ../app/views/layouts/admin_nav.php
+DEBUG - 2009-05-30 13:18:38 --> File loaded: ../app/views/layouts/admin_footer.php
+DEBUG - 2009-05-30 13:18:38 --> File loaded: ../app/views/admin/index.php
+DEBUG - 2009-05-30 13:18:38 --> File loaded: ../app/views/layouts/admin_page.php
+DEBUG - 2009-05-30 13:18:38 --> Final output sent to browser
+DEBUG - 2009-05-30 13:18:38 --> Total execution time: 0.0923
