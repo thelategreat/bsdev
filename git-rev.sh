735e5407b10e3486ed313fe41b8a83cf8d0a3830
J Knight <emptyvee@gmail.com>
2010-10-07 15:40:14 -0400

    - meh

--- git-rev.sh
+++ git-rev.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#
+# this can run on the client when pulling using .git/hooks/post-merge
+# i.e.
+# --[snip]--
+# if [ -f ./git-rev.sh ] 
+# then
+#   ./git-rev.sh > build.txt
+# fi
+# --[snip]--
+#
 
 GIT=git
 
