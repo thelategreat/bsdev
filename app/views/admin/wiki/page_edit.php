a9cc2e161585b7d9d84e93cae11cd369e44b475b
J Knight <emptyvee@gmail.com>
2010-11-22 13:15:41 -0500

    wiki tweaks

--- app/views/admin/wiki/page_edit.php
+++ app/views/admin/wiki/page_edit.php
@@ -48,5 +48,6 @@
   <input type="hidden" name="id" value="<?=$page->id?>" />
   <input name="title" value="<?=$page->title?>" /><br/>
   <textarea class="mceNoEditor" name="bodytext" cols="70" rows="20"><?=$page->body?></textarea><br/>
-  <input type="submit" name="save" value="Save" />
+  <input class="ok" type="submit" name="save" value="Save" />
+  <input type="submit" name="cancel" value="Cancel" />
 </form>
