601d661b5c245a1de617b310737d1ba18ac95c14
J Knight <emptyvee@gmail.com>
2010-11-24 13:04:43 -0500

    menu plugin tweaks

--- public/js/jquery.jdropdown.js
+++ public/js/jquery.jdropdown.js
@@ -1,4 +1,9 @@
-/* jQuery dropdown menu function
+/* jQuery dropdown multi-level menu function
+ *
+ * @author: J Knight <jim AT j2mfk DOT com>
+ *
+ * Example
+ *
  * <div id="nav">
  * <ul class="dropdown">
  *   <li><a href="...">Foo<a>
@@ -10,6 +15,19 @@
  * </ul>
  * </div>
  * $('#nav').dropDown({});
+ *
+ * Copyright 2010 J Knight (http://j2mfk.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 (function($) {
   // default values
@@ -37,14 +55,14 @@
           // show the submenu
           switch( opts.effect ) {
             case 'slide':
-              $(this).parent().find("ul.submenu").slideDown(opts.speed);
-            break;
+              $(this).parent().find("> ul.submenu").slideDown(opts.speed);
+              break;
             case 'fade':
-              $(this).parent().find("ul.submenu").fadeIn(opts.speed);
-            break;
+              $(this).parent().find("> ul.submenu").fadeIn(opts.speed);
+              break;
             default:
-              $(this).parent().find("ul.submenu").show();
-            break;
+              $(this).parent().find("> ul.submenu").show(opts.speed);
+              break;
           }
 
           // hover
@@ -57,13 +75,13 @@
               switch( opts.effect ) {
                 case 'slide':
                   $(this).parent().find("ul.submenu").slideUp(opts.speed);
-                break;
+                  break;
                 case 'fade':
                   $(this).parent().find("ul.submenu").fadeOut(opts.speed);
-                break;
+                  break;
                 default:
-                  $(this).parent().find("ul.submenu").hide();
-                break;
+                  $(this).parent().find("ul.submenu").hide(opts.speed);
+                  break;
               }
             }
           );
@@ -82,10 +100,30 @@
       $('ul.dropdown > li > ul.submenu > li').hover(
         function() { // on hover
           $(this).find("ul.submenu").css('left', 40);
-          $(this).find("ul.submenu").slideDown(opts.speed);
+          switch( opts.effect ) {
+            case 'slide':
+              $(this).find("ul.submenu").slideDown(opts.speed);
+              break;
+            case 'fade':
+              $(this).find("ul.submenu").fadeIn(opts.speed);
+              break;
+            default:
+              $(this).find("ul.submenu").show(opts.speed);
+              break;
+          }
         },
         function() { // on hover out
-          $(this).find("ul.submenu").slideUp(opts.speed);
+          switch( opts.effect ) {
+            case 'slide':
+              $(this).find("ul.submenu").slideUp(opts.speed);
+              break;
+            case 'fade':
+              $(this).find("ul.submenu").fadeOut(opts.speed);
+              break;
+            default:
+              $(this).find("ul.submenu").hide(opts.speed);
+              break;
+          }
       });
     }
   });
