e6e756f140fc0c43628579e50774c72ccf831232
J Knight <emptyvee@gmail.com>
2010-11-24 09:12:12 -0500

    footer tweak

--- app/views/layouts/standard_footer.php
+++ app/views/layouts/standard_footer.php
@@ -3,7 +3,7 @@
 -->
 <ul>
   <?php foreach( $pages[0]->children as $page ) { ?>
-    <li><a href='/page/<?=$page->title?>'/><?=$page->title?></a></li>
+    <li><a href='/page/view/<?=$page->id?>'/><?=$page->title?></a></li>
   <?php } ?>
 </ul>
 <!--
