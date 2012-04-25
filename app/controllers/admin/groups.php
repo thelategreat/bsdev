438bd37e0e444357d9f936317e9eca8011cd6ec9
j knight <jim@talonedge.com>
2012-04-25 15:11:29 -0400

    tweak tree view

--- app/controllers/admin/groups.php
+++ app/controllers/admin/groups.php
@@ -27,7 +27,7 @@ class Groups extends Admin_Controller
 		// don't show root item
 		$data['tree'] = $data['tree'][0]->children;
 		
-		$this->gen_page('Admin - Groups', 'admin/groups/groups_list', $data, '<h3>Sidebar Test</h3>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque a massa. Praesent facilisis viverra diam. Nulla auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque ultricies neque vitae nisl. Pellentesque ullamcorper est vitae eros. Ut nisi eros, mattis nec, tempus quis, rutrum eu, enim. Vivamus pellentesque dui nec lectus facilisis luctus. Donec mi libero, viverra sed, commodo sit amet, tincidunt id, sem. Duis turpis. Phasellus tristique, felis et interdum luctus, magna purus porttitor dui, eu viverra pede lectus eget elit. Mauris ultricies pellentesque turpis. Praesent tortor ante, adipiscing vel, ultricies eget, ullamcorper non, leo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus tincidunt consectetuer ligula. Quisque nibh. Phasellus ac urna in mauris bibendum pharetra. Nulla faucibus, diam eget ornare commodo, leo erat laoreet turpis, scelerisque interdum ipsum sapien at nulla.<hr/>' );
+    $this->gen_page('Admin - Groups', 'admin/groups/groups_list', $data );
 	}
 	
 	function add()
