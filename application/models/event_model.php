6c061f16ca55645d36e406ca30fcab802f80ab95
Robin de Bled <robin.debled@gmail.com>
2013-02-18 00:24:49 -0500

    Changed sql query - live server appeared to be case sensitive in table names

--- application/models/event_model.php
+++ application/models/event_model.php
@@ -1,21 +1,21 @@
 <?php
-if (!defined('BASEPATH')) exit('No direct script access allowed');   
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * 
+ *
  */
 class event_model extends CI_Model
 {
 	/**
-	 * 
+	 *
 	 */
 	function __construct()
 	{
 		parent::__construct();
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function get_event( $id )
 	{
@@ -23,9 +23,9 @@ class event_model extends CI_Model
 		$this->db->join('event_audience', 'events.audience = event_audience.id');
 		return $this->db->get('events');
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function get_event_media( $id )
 	{
@@ -36,32 +36,32 @@ class event_model extends CI_Model
 	function get_next_events( $count = 10 )
 	{
 		$query = "SELECT
-					EVENTS .id,
-					EVENTS .title,
-					EVENTS .dt_start,
+					events.id,
+					events.title,
+					events.dt_start,
 					media.uuid
 				FROM
-					EVENTS
-				LEFT JOIN media_map ON media_map.path = concat('/event/', EVENTS .id)
+					events
+				LEFT JOIN media_map ON media_map.path = concat('/event/', events.id)
 				LEFT JOIN media ON media.id = media_map.media_id
 				WHERE
 					TRUE
 				AND dt_start > now()
 				ORDER BY
-					EVENTS .dt_start
+				    events.dt_start
 				LIMIT $count";
 
 		return $this->db->query( $query );
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function get_events( $filter )
 	{
-		$query = "SELECT e.id, e.created_on, e.updated_on, e.submitter_id, e.title, e.venue, e.dt_start, e.dt_end, e.body, e.rating, ec.category, e.audience, e.event_ref, e.venue_ref "; 
+		$query = "SELECT e.id, e.created_on, e.updated_on, e.submitter_id, e.title, e.venue, e.dt_start, e.dt_end, e.body, e.rating, ec.category, e.audience, e.event_ref, e.venue_ref ";
 		$query .= " FROM events AS e, event_categories as ec WHERE e.category = ec.id ";
-		
+
 		switch( $filter['view']) {
 			case 'day':
 			$start = sprintf('%04d-%02d-%02d', $filter['year'],$filter['month'],$filter['day']);
@@ -76,62 +76,62 @@ class event_model extends CI_Model
 			$start = sprintf('%04d-%02d-%02d', $filter['year'],$filter['month'],1);
 			$query .= " AND dt_start BETWEEN DATE('$start') AND DATE_ADD(DATE('$start'), INTERVAL + 1 month)";
 		}
-		
+
 		/*
 		$start = sprintf('%04d-%02d-%02d', $filter['year'],$filter['month'],$filter['day']);
 		$end = sprintf('%04d-%02d-%02d', $filter['year'],$filter['month'],$filter['day']);
 		$view = $this->db->escape_str($filter['view']);
-		
+
 		$query =<<<EOF
-SELECT * FROM events 
+SELECT * FROM events
 	WHERE dt_start BETWEEN date('$start') AND DATE_ADD(DATE('$end'), INTERVAL + 1 $view)
 	ORDER BY dt_start ASC
 EOF;
 
 		*/
-		
+
 		$query .= " ORDER BY dt_start ASC";
-		
+
 		return $this->db->query( $query );
 	}
-	
+
 	/**
-	 * date in long format 
+	 * date in long format
 	 */
 	function get_events_by_date_range( $start, $end )
 	{
 		$start = date('Y-m-d', $start);
 		$end = date('Y-m-d', $end);
-		
+
 		$query =<<<EOF
-SELECT * FROM events 
+SELECT * FROM events
 	WHERE dt_start BETWEEN date('$start') AND DATE('$end')
 	ORDER BY dt_start ASC
 EOF;
 		return $this->db->query( $query );
 	}
-	
+
 	/**
 	 * Search from the front end
 	 */
 	function search_events($q, $page = 1, $limit = 10 )
-	{		
+	{
 		$term = $this->db->escape_like_str($q);
 		$offs = (($page - 1) * $limit);
-		
+
 		$sql =<<<EOF
 			SELECT e.id, e.title, e.dt_start, e.audience, ea.audience as audience_name, e.category, ec.category AS category_name
-			FROM events as e, event_audience as ea, event_categories as ec 
+			FROM events as e, event_audience as ea, event_categories as ec
 				WHERE (title LIKE '%$term%' OR body LIKE '%$term%')
 				AND ea.id = e.audience AND ec.id = e.category
 				LIMIT $limit OFFSET $offs
 EOF;
 
-		return $this->db->query( $sql );		
+		return $this->db->query( $sql );
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function add_event( $data )
 	{
@@ -148,13 +148,13 @@ EOF;
 		$this->db->set('dt_end', $data['dt_end']);
 		$this->db->set('created_on', 'NOW()', false );
 		$this->db->set('updated_on', 'NOW()', false );
-		
+
 		$this->db->insert('events');
 		return $this->db->insert_id();
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function update_event( $id, $data )
 	{
@@ -168,26 +168,26 @@ EOF;
 		$this->db->set('dt_start', $data['dt_start']);
 		$this->db->set('dt_end', $data['dt_end']);
 		$this->db->set('updated_on', 'NOW()', false );
-		
+
 		$this->db->where('id', $id );
 		$this->db->update('events' );
-		
+
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function delete_event( $data )
 	{
 		$this->db->where('id', $data['id']);
 		$this->db->delete( 'events' );
 	}
-	
+
 	function get_future_dates( $category, $title )
 	{
 		return $this->db->query("SELECT * FROM events WHERE category = $category AND title = " . $this->db->escape($title). " AND dt_start >= NOW()");
 	}
-	
+
 	function get_extra_info( $event )
 	{
 		$extra = array();
@@ -197,15 +197,15 @@ EOF;
 			$res = $this->db->query('SELECT director, country, year, rating, running_time, imdb_link FROM films WHERE id = ' . $event->event_ref );
 			foreach( $res->result_array() as $row ) {
 				foreach( $row as $k => $v ) {
-					$extra[$k] = $v;					
+					$extra[$k] = $v;
 				}
 			}
 			break;
 		}
-		
+
 		return $extra;
 	}
-	
+
 	function get_categories( )
 	{
 		return $this->db->query("SELECT * FROM event_categories");
@@ -213,7 +213,7 @@ EOF;
 
 	function get_audiences( )
 	{
-		return $this->db->query("SELECT * FROM event_audience");		
+		return $this->db->query("SELECT * FROM event_audience");
 	}
-	
+
 }
