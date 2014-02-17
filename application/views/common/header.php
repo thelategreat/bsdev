21cf2685ce1c223187d3a2abd03329f5e375de70
Robin de Bled <robin.debled@gmail.com>
2014-02-16 22:59:59 -0600

    Added dynamic routing to nav links
    Nav menu now uses dynamic routes as defined in the section manager
    where available.

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -69,9 +69,13 @@
             <li class="<? if (isset($page) && ( $it->id == $page->id || in_array($page->id, $it->child_ids) ) ) echo 'active' ?>"><a href="<? echo base_url('/section/view/' . $it->id);?>"><?=$it->name;?></a>
             <? if (isset($it->children) && count($it->children) > 0) { ?>
               <ul>
-              <? foreach ($it->children as $sub) { ?>
-                <li><a href="<? echo base_url('/section/view/' . $sub->id);?>"><?=$sub->name;?></a></li>
-              <? } ?>
+              <? foreach ($it->children as $sub) { 
+                  if (isset($sub->route) && ($sub->route != null)) {
+                    ?> <li><a href="<? echo base_url($sub->route); ?>"><?=$sub->name;?></a></li> <?
+                  } else {
+                    ?> <li><a href="<? echo base_url('/section/view/' . $sub->id);?>"><?=$sub->name;?></a></li>  <?
+                }
+             } ?>
               </ul>
             </li>
             <? 
