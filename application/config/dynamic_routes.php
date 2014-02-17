c11d3be1062e8f7fe9fb68f2c7dfaf96d63a377c
Robin de Bled <robin.debled@gmail.com>
2014-02-16 23:14:50 -0600

    Added dynamic route file to ignore list

--- application/config/dynamic_routes.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route[ 'cinemablog' ] = '/section/view/2';
-$route[ 'cinemablog/:any' ] = '/section/view/2';
-$route[ 'hotornot' ] = '/section/view/68';
-$route[ 'hotornot/:any' ] = '/section/view/68';
